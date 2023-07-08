6e7587029630db3a890ec72a70378f80aa5a71f8
Dante Vicenzo <dantevcz@gmail.com>
2023-07-08 07:38:10 -0300

    Add new product form validation

--- src/app/products/add/page.tsx
+++ src/app/products/add/page.tsx
@@ -1,29 +1,121 @@
+'use client'
+
 import { Button } from '@/components/button'
 import styles from './page.module.css'
 
 import Input from '@/components/input'
 import Textarea from '@/components/textarea'
-import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+const addNewProductFormValidationSchema = z.object({
+  url: z
+    .string()
+    .nonempty({
+      message: 'O campo imagem é obrigatório.',
+    })
+    .url({
+      message: 'Insira uma URL válida.',
+    }),
+  category: z.string().nonempty({
+    message: 'O campo categoria é obrigatório.',
+  }),
+  name: z
+    .string()
+    .nonempty({
+      message: 'O campo nome é obrigatório.',
+    })
+    .max(20, {
+      message:
+        'Limite de caracteres excedido. Por favor, insira no máximo 20 caracteres.',
+    }),
+  price: z.number({
+    required_error: 'O campo preço é obrigatório',
+  }),
+  description: z
+    .string()
+    .nonempty({
+      message: 'O campo descrição é obrigatório.',
+    })
+    .max(150, {
+      message:
+        'Limite de caracteres excedido. Por favor, insira no máximo 150 caracteres.',
+    }),
+})
+
+type TAddNewProductFormData = z.infer<typeof addNewProductFormValidationSchema>
 
 export default function AddNewProduct() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<TAddNewProductFormData>({
+    resolver: zodResolver(addNewProductFormValidationSchema),
+    mode: 'all',
+  })
+
+  function onSubmit(formData: TAddNewProductFormData) {
+    reset()
+  }
+
   return (
     <div className={styles.container}>
-      <form action="submit" className={styles.form}>
+      <form
+        action="submit"
+        className={styles.form}
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h1>Adicionar novo produto</h1>
-        <Input id="url" placeholder="URL da imagem" />
-        <Input id="category" placeholder="Categoria" />
-        <Input id="name" placeholder="Nome do produto" />
-        <Input id="price" placeholder="Preço do produto" />
-        <Textarea id="description" placeholder="Descrição do produto" />
-        <Link href="/products">
-          <Button
-            variantColor="blue"
-            variantPadding="fill"
-            responsivity={false}
-          >
-            Adicionar produto
-          </Button>
-        </Link>
+        <Input
+          {...register('url', { required: true })}
+          placeholder="URL da imagem"
+        />
+        {errors.url && (
+          <span className={styles.error}>{errors.url.message}</span>
+        )}
+        <Input
+          {...register('category', { required: true })}
+          placeholder="Categoria"
+        />
+        {errors.category && (
+          <span className={styles.error}>{errors.category.message}</span>
+        )}
+        <Input
+          {...register('name', { required: true, maxLength: 20 })}
+          placeholder="Nome do produto"
+        />
+        {errors.name && (
+          <span className={styles.error}>{errors.name.message}</span>
+        )}
+        <Input
+          {...register('price', {
+            required: true,
+            setValueAs: (v) => (v === '' ? undefined : parseFloat(v)),
+          })}
+          type="number"
+          placeholder="Preço do produto"
+        />
+        {errors.price && (
+          <span className={styles.error}>{errors.price.message}</span>
+        )}
+        <Textarea
+          {...register('description', { required: true, maxLength: 150 })}
+          placeholder="Descrição do produto"
+        />
+        {errors.description && (
+          <span className={styles.error}>{errors.description.message}</span>
+        )}
+        <Button
+          type="submit"
+          variantColor="blue"
+          variantPadding="fill"
+          responsivity={false}
+        >
+          Adicionar produto
+        </Button>
       </form>
     </div>
   )
