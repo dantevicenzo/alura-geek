3f09efff9dde6b00e63f57b1008145eb2ee0b301
Dante Vicenzo <dantevcz@gmail.com>
2023-07-09 00:49:01 -0300

    Verify window before access localstorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -4,17 +4,20 @@ import { IProduct } from '@/app/gallery'
 import { products as dbProducts } from '../app/db'
 
 export const useLocalStorage = () => {
-  const localStorageProducts = localStorage.getItem(
-    '@alura-geek:products-1.0.0',
-  )
-
-  if (localStorageProducts) {
-    return JSON.parse(localStorageProducts) as IProduct[]
-  } else {
-    localStorage.setItem(
+  if (typeof window !== 'undefined') {
+    const localStorageProducts = localStorage.getItem(
       '@alura-geek:products-1.0.0',
-      JSON.stringify(dbProducts),
     )
-    return dbProducts
+
+    if (localStorageProducts) {
+      return JSON.parse(localStorageProducts) as IProduct[]
+    } else {
+      localStorage.setItem(
+        '@alura-geek:products-1.0.0',
+        JSON.stringify(dbProducts),
+      )
+      return dbProducts
+    }
   }
+  return dbProducts
 }
